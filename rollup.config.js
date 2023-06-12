5c654a5e056f4a806c4681080920ef00af8cd4e0
Eyas Valdez <eyasvaldez7@gmail.com>
2023-06-11 23:25:04 -0400

    simplified bundling steps features: - add .build to .gitignore. modify .gitignore for the demo paths. - change the exports and export sequence according to TS docs (https://www.typescriptlang.org/docs/handbook/esm-node.html). - remove scripts based on new bundle steps: remove many build:* and clean:* scripts - changed the bundle strategy: 1. tsc transpile src contents to .build as ESM 2. use Rollup to bundle ESM and transpile to CJS (note: no longer splitting directories) 3. use Rollup to bundle type module into the .build directory 4. use Rollup to bundle the types modules in .build with the .build/index.d.ts entry - removed tsconfig.base/cjs.json because transpiling happens once now - removed rollup.types.js because both code and type bundling happens in rollup.config.js now - remove unused @rollup/plugin-typescript dependency and utilize rollup-plugin-dts instead - use package.json in rollup.config.js (so that in future I won't have to change the output files twice) - added specificity to mv script in demo package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,46 @@
-import typescript from '@rollup/plugin-typescript';
+import package_details from './package.json'
+import dts from 'rollup-plugin-dts'
+
 export default [
 	{
-		input: 'src/index.ts',
-		output: {
-			file: 'dist/mjs/index.mjs',
-			format: 'es',
-			exports: 'named',
-			generatedCode: {
-				constBindings: true
+		input: './.build/index.js',
+		output: [
+			{
+				file: package_details.exports['.'].import.default,
+				format: 'es',
+				exports: 'named',
+				generatedCode: {
+					constBindings: true
+				}
+			},
+			{
+				file: package_details.exports['.'].require.default,
+				format: 'commonjs',
+				exports: 'named',
+				generatedCode: {
+					constBindings: true
+				}
+			},
+		],
+	},
+	{
+		input: './src/@types/index.d.ts',
+		output: [
+			{
+				file: './.build/@types/index.d.ts',
+				format: 'es'
 			}
-		},
-		plugins: [typescript()]
+		],
+		plugins: [dts.default()],
+	},
+	{
+		input: './.build/index.d.ts',
+		output: [
+			{
+				file: package_details.types,
+				format: 'es'
+			}
+		],
+		plugins: [dts.default()],
 	}
 ]
